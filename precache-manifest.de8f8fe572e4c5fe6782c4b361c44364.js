f5dea76612243b40ac02773c9314a098dfa091a3
Yordi Ogando <yordiogando@gmail.com>
2020-12-03 18:30:43 -0400

    Updates

---
+++ precache-manifest.de8f8fe572e4c5fe6782c4b361c44364.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e607b1eba50ab2d8018e9ee10367c336",
+    "url": "/sls-course-frontend/index.html"
+  },
+  {
+    "revision": "e3bff6fe86c48328512f",
+    "url": "/sls-course-frontend/static/css/main.028e3cb6.chunk.css"
+  },
+  {
+    "revision": "e4f6731b32e93e7109ec",
+    "url": "/sls-course-frontend/static/js/2.95cc098b.chunk.js"
+  },
+  {
+    "revision": "5727441246ffa49345a3574f984c2034",
+    "url": "/sls-course-frontend/static/js/2.95cc098b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3bff6fe86c48328512f",
+    "url": "/sls-course-frontend/static/js/main.f9636cb5.chunk.js"
+  },
+  {
+    "revision": "6536c8d978b90d7d0f4f",
+    "url": "/sls-course-frontend/static/js/runtime-main.1e92cc17.js"
+  }
+]);
