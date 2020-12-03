94abf010bf02e3425ff9ad4d5d20388c61a99e25
Yordi Ogando <yordiogando@gmail.com>
2020-12-03 18:22:54 -0400

    UI changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,31 +1,32 @@
 import React from "react";
 import { useAuth0 } from "../react-auth0-spa";
-import { inject } from 'mobx-react';
-import { makeStyles, Button } from '@material-ui/core';
+import { inject } from "mobx-react";
+import { makeStyles, Button } from "@material-ui/core";
 
 const useStyles = makeStyles({
   navbar: {
-    background: 'linear-gradient(90deg, rgba(190,52,32,1) 0%, rgba(231,75,77,1) 48%, rgba(231,148,74,1) 100%)',
+    background:
+      "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(4,19,62,1) 2%, rgba(6,31,80,1) 4%, rgba(7,44,98,1) 8%, rgba(9,61,121,1) 29%, rgba(7,89,145,1) 53%, rgba(5,129,181,1) 86%, rgba(0,212,255,1) 100%)",
     padding: 14,
     marginBottom: 24,
-    display: 'flex',
-    width: '100%',
-    boxSizing: 'border-box',
+    display: "flex",
+    width: "100%",
+    boxSizing: "border-box",
   },
   header: {
-    flexBasis: '50%',
-    display: 'flex'
+    flexBasis: "50%",
+    display: "flex",
   },
   loginLogoutContainer: {
-    display: 'flex',
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-    flexBasis: '50%',
+    display: "flex",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    flexBasis: "50%",
   },
   button: {
-    color: 'white',
+    color: "white",
   },
-})
+});
 
 const NavBar = ({ authStore }) => {
   const auth0 = useAuth0();
@@ -37,7 +38,7 @@ const NavBar = ({ authStore }) => {
   return (
     <div className={classes.navbar}>
       <div className={classes.header}>
-        <h1 style={{ fontSize: 14, color: 'white' }}>THE AUCTION HOUSE</h1>
+        <h1 style={{ fontSize: 14, color: "white" }}>YD AUCTIONS</h1>
       </div>
       <div className={classes.loginLogoutContainer}>
         {!isAuthenticated && (
@@ -45,15 +46,12 @@ const NavBar = ({ authStore }) => {
             className={classes.button}
             onClick={() => loginWithRedirect({})}
           >
-           Sign in
+            Sign in
           </Button>
         )}
 
         {isAuthenticated && (
-          <Button
-            className={classes.button}
-            onClick={() => logout({})}
-          >
+          <Button className={classes.button} onClick={() => logout({})}>
             Sign out
           </Button>
         )}
@@ -62,4 +60,4 @@ const NavBar = ({ authStore }) => {
   );
 };
 
-export default inject(['authStore'])(NavBar);
+export default inject(["authStore"])(NavBar);
